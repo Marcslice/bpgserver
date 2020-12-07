4ce219a7e51080b6ab72c47514b2728a548718da
lPraxy <praxydev@gmail.com>
2020-12-06 20:20:18 -0500

    fix use skill

--- UseSkill.sql
+++ UseSkill.sql
@@ -2,23 +2,32 @@ CREATE PROCEDURE UseSkill(@playerId int)
 AS
 BEGIN
     DECLARE @skillId INT
+    DECLARE @skillCost INT
+    DECLARE @playerMP INT
 
     SELECT @skillId = playerStatusSkillId FROM playerStatus WHERE playerStatusPlayerId = @playerId
+    SELECT @playerMP = playerCurrentMP FROM Player WHERE playerId = @playerId
+    SELECT @skillCost = skillManaCost FROM Skill WHERE skillId = @skillId
 
-    IF @skillId = 1
+    IF (@playerMP >= @skillCost)
     BEGIN
-        EXEC dbo.ArcaneShot @playerId = @playerId
+        IF @skillId = 1
+        BEGIN
+            EXEC dbo.ArcaneShot @playerId = @playerId
+        END
+        ELSE IF @skillId = 2
+        BEGIN
+            EXEC dbo.HealingPrayer @playerId = @playerId
+        END
+        ELSE IF @skillId = 3
+        BEGIN
+            EXEC dbo.FlurryOfBlows @playerId = @playerId
+        END
+        ELSE IF @skillId = 4
+        BEGIN
+            EXEC dbo.Smite @playerId = @playerId
+        END
     END
-    ELSE IF @skillId = 2
-    BEGIN
-        EXEC dbo.HealingPrayer @playerId = @playerId
-    END
-    ELSE IF @skillId = 3
-    BEGIN
-        EXEC dbo.FlurryOfBlows @playerId = @playerId
-    END
-    ELSE IF @skillId = 4
-    BEGIN
-        EXEC dbo.Smite @playerId = @playerId
-    END
+    ELSE
+        EXEC dbo.PlayerAtk @playerId = @playerId
 END
